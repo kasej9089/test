ebd247de86ecb777278896a09b0afffb6ecba1f1
kasej9089 <kasej9089@gmail.com>
2025-01-14 11:42:33 +0800

    test2

--- testfolder/test2/obj/Debug/net8.0/test2.AssemblyInfo.cs
+++ testfolder/test2/obj/Debug/net8.0/test2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed3610215f1205ddbab95170219fcb1e4eebc39c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45336306f095c302b2aea0e18f9ce54001680d33")]
 [assembly: System.Reflection.AssemblyProductAttribute("test2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
